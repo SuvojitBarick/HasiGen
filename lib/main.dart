5ae6dd05843b8ffde502a2e188dbef07988a0e79
Ayush Kejariwal <kejayush29@gmail.com>
2020-04-26 19:58:16 +0530

    added preview image and copyable text

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
 
 class MyAppHome extends State<MyApp> {
   var _str;
-
+  var imgfile;
   @override
   void initState() {
     super.initState();
@@ -26,6 +26,7 @@ class MyAppHome extends State<MyApp> {
     ImagePicker.pickImage(source: ImageSource.gallery).then(
       (image) => setState(
         () {
+          imgfile = image;
           requesting(image);
         },
       ),
@@ -34,8 +35,7 @@ class MyAppHome extends State<MyApp> {
 
   void requesting(File image) async {
     String keys = "";
-    final FirebaseVisionImage visionImage =
-        FirebaseVisionImage.fromFile(image);
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
     final ImageLabeler labeler = FirebaseVision.instance.imageLabeler();
     final List<ImageLabel> labels = await labeler.processImage(visionImage);
     for (ImageLabel label in labels) {
@@ -57,21 +57,70 @@ class MyAppHome extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hashigen"),
+          title: Text(
+            "Hashigen",
+            style: TextStyle(fontSize: 24),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              RaisedButton(
-                child: Icon(Icons.add_a_photo),
-                onPressed: getImage,
-              ),
-              Card(
-                child:
-                    (_str != null) ? Text(_str) : CircularProgressIndicator(),
-              ),
-            ],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    'Tap below icon to chose your image',
+                  ),
+                ),
+                RaisedButton(
+                  child: Icon(Icons.add_a_photo),
+                  onPressed: getImage,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: (_str != null)
+                      ? Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 200,
+                              width: 200,
+                              child: imgfile != null
+                                  ? Image.file(
+                                      imgfile,
+                                      fit: BoxFit.contain,
+                                    )
+                                  : Icon(
+                                      Icons.image,
+                                      size: 100,
+                                    ),
+                            ),
+                            Divider(),
+                            SelectableText(
+                              '$_str',
+                              // maxLines: 10,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.blueAccent,
+                              ),
+                              toolbarOptions: ToolbarOptions(
+                                  copy: true,
+                                  selectAll: true,
+                                  cut: false,
+                                  paste: false),
+                            )
+                          ],
+                        )
+                      : imgfile == null
+                          ? Center(
+                              child: Text('input an image to get you hashtags'))
+                          : CircularProgressIndicator(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
